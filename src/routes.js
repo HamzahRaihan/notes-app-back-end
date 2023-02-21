fd525895922551490059f61e27a8858004d37840
HamzahRaihan <hamzahraihanif@gmail.com>
2023-02-21 16:48:14 +0700

    update: add notes

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,20 @@
+const { addNoteHandler, getAllNotesHandler, getNoteByIdHandler } = require('./handler');
+
 const routes = [
   {
-    method: "POST",
-    path: "/notes",
-    handler: () => {},
+    method: 'POST',
+    path: '/notes',
+    handler: addNoteHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNoteByIdHandler,
   },
 ];
 
